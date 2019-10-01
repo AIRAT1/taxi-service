406c18d1ccedd0b28323aac1116fdff4bb33ed3f
AIRAT1 <ayrat1@mail.ru>
2019-10-01 21:28:14 +0300

    all tests passed

--- src/test/java/TaxiServiceTest.java
+++ src/test/java/TaxiServiceTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaxiServiceTest {
+    @Test
+    void calculate() {
+        TaxiService service = new TaxiService();
+        int calculateValue = service.calculate(100);
+        assertEquals(1960, calculateValue);
+    }
 
+    @Test
+    void calculateIncorrectValue() {
+        TaxiService service = new TaxiService();
+        int calculate = service.calculate(-5);
+        assertEquals(0, calculate);
+    }
 }
